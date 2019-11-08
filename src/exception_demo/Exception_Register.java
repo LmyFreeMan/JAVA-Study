ed9241059eeff5bcb0e23908ae2e6a9a0e61eaa8
Coder <2515946609@qq.com>
2019-11-08 15:22:53 +0800

    java异常机制

--- src/exception_demo/Exception_Register.java
+++ src/exception_demo/Exception_Register.java
@@ -1,17 +1,13 @@
 package exception_demo;
-
-
 /**
  * @author Coder
  *
  */
 public class Exception_Register extends RuntimeException {
-
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1644805913114434501L;
-
 	/**
 	 * @param message 抛出异常的信息
 	 */
@@ -19,7 +15,6 @@ public class Exception_Register extends RuntimeException {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
-
 	/**
 	 * @param message 抛出异常的信息
 	 * @param cause   抛出异常的根本原因
@@ -28,10 +23,7 @@ public class Exception_Register extends RuntimeException {
 		// TODO Auto-generated constructor stub
 		super(message, cause);
 	}
-
-
 	public Exception_Register() {
 		// TODO Auto-generated constructor stub
 	}
-
 }
