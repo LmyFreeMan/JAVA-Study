7e362c9169be569533797135a5a39bf1ca7367e3
Coder <2515946609@qq.com>
2019-11-08 15:22:26 +0800

    java日期练习

--- src/date_demo/Date_Demo.java
+++ src/date_demo/Date_Demo.java
@@ -1,11 +1,9 @@
 package date_demo;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.Date;
-
 public class Date_Demo {
     public static void main(String[] args) {
         Date date=new Date();
@@ -23,17 +21,13 @@ public class Date_Demo {
         System.out.println("当前时间"+time);//当前时间10:54:54.421
         LocalTime specificTime = LocalTime.of(12,20,25,40);
         System.out.println("特定时间"+specificTime);//特定时间12:20:25.000000040
-
         //拿到其他时区时间
         LocalTime time2 = LocalTime.now(ZoneId.of("UTC+2"));
         System.out.println("东二区现在"+time2);//东二区现在04:55:32.537
-
         LocalTime specificSecondTime = LocalTime.ofSecondOfDay(10000);
         System.out.println("零点后一万秒"+specificSecondTime);//零点后一万秒02:46:40
-
         //Current Date
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("Current DateTime="+localDateTime);
-
     }
 }
