f6594f938751ac0ef46e0486888c81bd9496c5cd
Coder <2515946609@qq.com>
2019-11-12 17:32:13 +0800

    Vector_练习

--- src/vector_demo/Vector_Demo.java
+++ src/vector_demo/Vector_Demo.java
@@ -1,15 +1,22 @@
 package vector_demo;
 
+import java.util.Enumeration;
 import java.util.Vector;
-
 public class Vector_Demo {
     public static void main(String[] args) {
         Vector vector = new Vector(5);
+        //增加元素
         vector.add("A");
         vector.add("B");
         vector.add("C");
         System.out.println(vector);
+        //修改元素
         vector.set(2, "H");
         System.out.println(vector);
+        //遍历元素
+        Enumeration en = vector.elements();
+        while (en.hasMoreElements()) {
+            System.out.println(en.nextElement());
+        }
     }
 }
