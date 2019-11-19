497304de7fa18f55b1574a81d67e5d4c5eac81ab
Coder <2515946609@qq.com>
2019-11-19 10:32:10 +0800

    Array_练习

--- src/array_demo/Array_Demo.java
+++ src/array_demo/Array_Demo.java
@@ -1,6 +1,11 @@
 package array_demo;
+
 import org.apache.commons.lang.ArrayUtils;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 public class Array_Demo {
 	public Array_Demo() {
 		// TODO Auto-generated constructor stub
@@ -8,9 +13,14 @@ public class Array_Demo {
     public static void main(String[] args) {
     	//arraycopy将指定源数组中的数组从指定位置复制到目标数组的指定位置
 		int[] name= new int[] {1,3,5,7};
-		System.out.println(Arrays.toString(name));
+
+        System.out.println(Arrays.toString(name));
 		int[] src= {1,2,3,4};
 		int[] dest= {4,5,6,7};
+        //数组转化为List对象,定长,不可修改，不能删除，不可添加，只可以修改。
+        List list = new ArrayList();
+        list = Arrays.asList("A", "B", "C", "D");
+        System.out.println(list);
 		System.arraycopy(src, 0, dest, 0, 4);
 		System.out.println("这是一条华丽的分割线");
 		System.out.println("这是一条华丽的分割线");
