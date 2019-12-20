eb2e3ba7b1eaa9d4d9256c390e7b39a26d729e9b
Coder <2515946609@qq.com>
2019-12-20 15:38:43 +0800

    User类

---
+++ src/bean_demo/User.java
@@ -0,0 +1,48 @@
+package bean_demo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private String password;
+    private int age;
+
+    public User(String name, String password, int age) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
